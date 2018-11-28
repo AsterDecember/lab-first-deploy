4b94b75405fe9e9e7ce800027c4a8ea9e9c89ae9
AsterDecember <aster.december@gmail.com>
2018-11-28 16:02:54 -0600

    final final

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-require('dontenv').config()
+require('dotenv').config()
 
-const express        = require("express");
 const express        = require("express");
 const session        = require("express-session");
 const MongoStore     = require("connect-mongo")(session);
